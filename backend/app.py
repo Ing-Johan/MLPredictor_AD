379506c0a872ab25bb1783ac892f3ce53c544fef
johan hernandez <johan.hernandezpe@campusucc.edu.co>
2026-05-04 10:33:32 -0500

    fix: configuracion para render

--- backend/app.py
+++ backend/app.py
@@ -204,5 +204,6 @@ def api_predict_batch():
 
 # ── Run ────────────────────────────────────────────────────────────────────────
 if __name__ == "__main__":
-    print("\nServidor iniciado en http://localhost:5000\n")
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    print(f"\nServidor iniciado en el puerto {port}\n")
+    app.run(host="0.0.0.0", port=port)
